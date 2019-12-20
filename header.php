e7a00e893f6fc6d6b609c21fc054be55449a1fef
longnhat131199 <57885692+longnhat131199@users.noreply.github.com>
2019-12-20 16:35:54 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,67 @@
+	<div id="header">
+
+			<div id="brand">
+				<a href="index.php">
+				<img class="brand" src="themes/_logo/logo.png" style="float:left; ">
+				
+			</div>
+
+		<nav id="sticky_wrapper" class="navbar navbar-expand-lg navbar-light bg-light|navbar-primary bg-primary|navbar-secondary bg-secondary|navbar-success bg-success|navbar-danger bg-danger|navbar-warning bg-warning|navbar-info bg-info|">
+
+			<h3><a href="index.php">ĐẠI HỌC THỦY LỢI</a></h3>
+		  <button class="navbar-toggler position-absolute bottom-menu-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-right: 40px !important; ">
+		    <ul class="navbar-nav ml-auto" style="line-height: 40px">
+		        <li class="nav-item">
+			        <a class="nav-link" href="#">TIN TỨC & THÔNG BÁO</a>
+			    </li>
+			    <li class="nav-item">
+			        <a class="nav-link" href="#">TUYỂN SINH</a>
+			    </li>
+			    <li class="nav-item">
+			        <a class="nav-link" href="#">LIÊN HỆ</a>
+			    </li>
+			    <li class="nav-item">
+			        <a class="nav-link" href="#">GIỚI THIỆU</a>
+			    </li>
+			    <li class="nav-item">
+			        <a class="nav-link" href="#">ĐÀO TẠO</a>
+				</li>
+				<li class="nav-item">
+			        <a class="nav-link" href="#">KHOA HỌC & CÔNG NGHỆ</a>
+			    </li>
+
+		    </ul>
+		  </div>
+		  
+		</nav>
+	</div>
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="themes/slide/daihoi.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="themes/slide/22-12.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="themes/slide/dangbo1.png" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
